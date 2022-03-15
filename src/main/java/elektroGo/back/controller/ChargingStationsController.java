c7cadcb310148546b2ae342aee738f874b8b4191
Marc Castells <marc.casellsd@gmail.com>
2022-03-15 13:55:15 +0100

    Creació Charging satation Controller

--- src/main/java/elektroGo/back/controller/ChargingStationsController.java
+++ src/main/java/elektroGo/back/controller/ChargingStationsController.java
@@ -1,13 +1,23 @@
 package elektroGo.back.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import elektroGo.back.data.Finders.FinderChargingStations;
+import elektroGo.back.data.Gateways.GatewayChargingStations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 @RestController
 public class ChargingStationsController {
 
-    @GetMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!\n";
+    @GetMapping("/ChargingStations")
+    public String getChargingStations() throws JsonProcessingException, SQLException {
+        FinderChargingStations fCS = FinderChargingStations.getInstance();
+        ArrayList<GatewayChargingStations> ArrayCS = fCS.findAll();
+        ObjectMapper ob = new ObjectMapper();
+        return ob.writeValueAsString(ArrayCS);
     }
 }
