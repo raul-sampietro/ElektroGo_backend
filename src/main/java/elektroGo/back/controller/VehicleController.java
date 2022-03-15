6b78536ce92f7f8a747a3b1ae95a2d9a59a03ad7
daniel.pulido.galvez <daniel.pulido.galvez@estudiantat.upc.edu>
2022-03-15 20:57:46 +0100

    Task#77_Afegeix el controller de vehicle amb metodes http de crear, llegir i eliminar

--- src/main/java/elektroGo/back/controller/VehicleController.java
+++ src/main/java/elektroGo/back/controller/VehicleController.java
@@ -1,21 +1,16 @@
 package elektroGo.back.controller;
 
-import elektroGo.back.data.Database;
 import elektroGo.back.data.Finders.FinderVehicle;
 import elektroGo.back.data.Gateways.GatewayVehicle;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 @RestController
 public class VehicleController {
 
     @PostMapping("/vehicle/create")
     public void createVehicle(@RequestBody GatewayVehicle gV) {
-        Database d = Database.getInstance();
         try {
             gV.insert();
         } catch (SQLException e) {
@@ -23,41 +18,29 @@ public class VehicleController {
         }
     }
 
-    @PostMapping("/vehicle/provaGetVehicle")
-    public String prova(@RequestBody GatewayVehicle gV) {
-        return gV.json();
-    }
-
-    @GetMapping("/vehicle/provaReadVehicle")
-    public String provaReadVehicle() {
-        Database d = Database.getInstance();
-        try {
-            d.executeSQLUpdate("insert into VEHICLE values(22,null,'testModel','5755',null,null,null,null,'Test');");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        FinderVehicle fV = FinderVehicle.getInstance();
-        GatewayVehicle gVTest = null;
-        try {
-            gVTest = fV.findByID(22);
-            d.executeSQLUpdate("delete from VEHICLE where id = 22;");
-            return gVTest.json();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
     @GetMapping("/vehicle/read")
     public String readVehicle(@RequestParam Long id) {
         FinderVehicle fV = FinderVehicle.getInstance();
         try {
             GatewayVehicle gV = fV.findByID(id);
+            if (gV == null) return null; //null is fine or show some http error or return something that front knows?
             return gV.json();
         }
         catch (SQLException e ) {
             return e.getSQLState();
         }
+    }
 
+    @PostMapping("/vehicle/delete")
+    public void deleteVehicle(@RequestParam Long id) {
+        FinderVehicle fV = FinderVehicle.getInstance();
+        try {
+            GatewayVehicle gV = fV.findByID(id);
+            if (gV != null) gV.remove();
+            //Otherwise, show some http error?
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
