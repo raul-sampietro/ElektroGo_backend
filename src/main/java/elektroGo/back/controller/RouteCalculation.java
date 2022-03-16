b997306ac1a56172a61f2ad212b2a53be5f12166
1251829 <adria.abad@estudiantat.upc.edu>
2022-03-16 14:10:39 +0100

    Task#94_Afegeix seleccio de punts de carrega viables per la ruta

--- src/main/java/elektroGo/back/controller/RouteCalculation.java
+++ src/main/java/elektroGo/back/controller/RouteCalculation.java
@@ -1,13 +1,29 @@
 package elektroGo.back.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 class Point {
     String name;
     Double lat;
     Double lon;
 }
 
+class Vector {
+    Double x;
+    Double y;
+}
+
+class Rectangle {
+    Point BL = new Point();
+    Point BR = new Point();
+    Point TL = new Point();
+    Point TR = new Point();
+}
+
 public class RouteCalculation {
-    static Point[] chargers;
+    static ArrayList<Point> chargers;
     static int stopsNeeded;
     static int range; //in kilometers
 
@@ -17,7 +33,7 @@ public class RouteCalculation {
         double a = 0.5 - Math.cos((x2 - x1) * p)/2 +
                 Math.cos(x1 * p) * Math.cos(x2 * p) *
                         (1 - Math.cos((y2 - y1) * p))/2;
-        return 2 * 6371 * Math.asin(Math.sqrt(a)); // Radio tierra = 6371 km
+        return Math.asin(Math.sqrt(a));
     }
 
     static double calculateRoadDistance() { //in kilometers
@@ -40,10 +56,41 @@ public class RouteCalculation {
          */
     }
 
-    static Point[] selectPossibleChargers(Point[] chargers, Point ori, Point dest) {
+    //Pensado para el rectangulo naive
+    static boolean inRectangle(Rectangle rectangle, Point point) {
+        System.out.println("       " + point.lat + ", " + point.lon);
+        if (point.lat > rectangle.TR.lat || point.lat < rectangle.BL.lat ||
+            point.lon > rectangle.BR.lon || point.lon < rectangle.BL.lon) return false;
+        return true;
+    }
 
+    static ArrayList<Point> selectPossibleChargers(ArrayList<Point> chargers, Point ori, Point dest) {
 
-        return null;
+        //Director vector, not in use right now
+        Vector vector = new Vector(); vector.x = ori.lon - dest.lon; vector.y = ori.lat - dest.lat;
+        System.out.println("Vector: x=" + vector.x + " y=" + vector.y);
+        Double module = Math.sqrt(Math.pow(vector.x, 2) + Math.pow(vector.y, 2));
+        System.out.println("Vector normalized: x=" + vector.x/module + " y=" + vector.y/module);
+
+        //Set rectangle area where chargers should be found
+        //First naive version, improve to an oriented rectangle between points
+        Rectangle rectangle = new Rectangle();
+        rectangle.BL.lat = Math.min(ori.lat, dest.lat); rectangle.BL.lon = Math.min(ori.lon, dest.lon);
+        rectangle.BR.lat = Math.min(ori.lat, dest.lat); rectangle.BR.lon = Math.max(ori.lon, dest.lon);
+        rectangle.TR.lat = Math.max(ori.lat, dest.lat); rectangle.TR.lon = Math.max(ori.lon, dest.lon);
+        rectangle.TL.lat = Math.max(ori.lat, dest.lat); rectangle.TL.lon = Math.min(ori.lon, dest.lon);
+
+        System.out.println("BL = " + rectangle.BL.lat + ", " + rectangle.BL.lon);
+        System.out.println("BR = " + rectangle.BR.lat + ", " + rectangle.BR.lon);
+        System.out.println("TL = " + rectangle.TL.lat + ", " + rectangle.TL.lon);
+        System.out.println("TR = " + rectangle.TR.lat + ", " + rectangle.TR.lon);
+
+        ArrayList<Point> candidates = new ArrayList<>();
+        for (Point p : chargers) {
+            System.out.println("Checking point: " + p.name);
+            if (inRectangle(rectangle, p)) candidates.add(p);
+        }
+        return candidates;
     }
 
     public static void main(String[] args) {
@@ -54,23 +101,32 @@ public class RouteCalculation {
         Point c5 = new Point(); c5.name = "Mala opcion"; c5.lat = 41.229957; c5.lon = 1.681126;
         Point c6 = new Point(); c6.name = "Girona"; c6.lat = 41.069389; c6.lon = 1.08736;
         Point c7 = new Point(); c7.name = "Andorra"; c7.lat = 42.497339; c7.lon = 1.507169;
-        Point c8 = new Point(); c8.name = "lleida"; c8.lat = 41.608993; c8.lon = 0.620819;
+        Point c8 = new Point(); c8.name = "Lleida"; c8.lat = 41.608993; c8.lon = 0.620819;
 
-        chargers = new Point[]{c1, c2, c3, c4, c5, c6, c7, c8};
+        Point[] list = new Point[]{c1, c2, c3, c4, c5, c6, c7, c8};
+        chargers = new ArrayList<>(List.of(list));
+
         Point ori = new Point(); ori.name = "Lloret de Mar"; ori.lat = 41.700801; ori.lon = 2.847613;
         Point dest = new Point(); dest.name = "Amposta"; dest.lat = 40.705865; dest.lon = 0.578646;
 
         range = 150;
-        double distance = calculateRawDistance(ori.lat, ori.lon, dest.lat, dest.lon);
-        System.out.println("Distance: " + distance);
+
+        double distance = calculateRawDistance(ori.lon, ori.lat, dest.lon, dest.lat);
+        System.out.println("Distance: " + String.format("%.02f", distance));
 
         stopsNeeded = (int) (distance/range);
         System.out.println("Stops needed: " + stopsNeeded);
 
         double roadDistance = calculateRoadDistance();
-        System.out.println("Road distance: " + roadDistance);
+        System.out.println("Road distance: " + roadDistance/1000 + " kilometers");
 
-        Point[] candidates = selectPossibleChargers(chargers, ori, dest);
+        //List of chargers to consider when computing route
+        ArrayList<Point> candidates = selectPossibleChargers(chargers, ori, dest);
 
+        /*
+            todo pedir a google la matriz de distancia por carretera entre todos los chargers y ori y dest
+            Dependiendo del numero de paradas, coger de la matriz las combinacions de origen-chargers-destino
+            que sean viables (autonomia y kilometros entre puntos) y de estas coger la mas corta en km
+        */
     }
 }
