4d06cc767528abb62850e0d287f3a1d1ab7e093b
daniel.pulido.galvez <daniel.pulido.galvez@estudiantat.upc.edu>
2022-03-17 23:05:59 +0100

    FixDriverVehicle_Implementa DriverVehicleTest i fa els canvis corresponents al Gateway, Finder i Test de Vehicle.

---
+++ src/test/java/elektroGo/back/DriverVehicleTest.java
@@ -0,0 +1,75 @@
+package elektroGo.back;
+
+import elektroGo.back.data.Finders.FinderDriverVehicle;
+import elektroGo.back.data.Finders.FinderVehicle;
+import elektroGo.back.data.Gateways.GatewayDriverVehicle;
+import elektroGo.back.data.Gateways.GatewayVehicle;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class DriverVehicleTest {
+
+    private void insertVehicle() throws SQLException {
+        GatewayVehicle gV = new GatewayVehicle("testBrand", "testModel", "TestV",
+                666, LocalDate.of(2010, 3, 22), 3);
+        gV.insert();
+    }
+
+    private void insertVehicle2() throws SQLException {
+        GatewayVehicle gV = new GatewayVehicle("testBrand", "testModel", "TestV2",
+                666, LocalDate.of(2010, 3, 22), 3);
+        gV.insert();
+    }
+
+    private GatewayDriverVehicle insertDriverVehicle() throws SQLException {
+        FinderVehicle fV = FinderVehicle.getInstance();
+        if (fV.findByNumberPlate("TestV") == null) insertVehicle();
+        GatewayDriverVehicle gDV = new GatewayDriverVehicle("TestV","Test");
+        gDV.insert();
+        return gDV;
+    }
+
+    private ArrayList<GatewayDriverVehicle> insertDriverVehicle2() throws SQLException {
+        FinderVehicle fV = FinderVehicle.getInstance();
+        if (fV.findByNumberPlate("TestV") == null) insertVehicle();
+        if (fV.findByNumberPlate("TestV2") == null) insertVehicle2();
+        GatewayDriverVehicle gDV = new GatewayDriverVehicle("TestV","Test");
+        GatewayDriverVehicle gDV2 = new GatewayDriverVehicle("TestV2","Test");
+        gDV2.insert();
+        gDV.insert();
+        ArrayList<GatewayDriverVehicle> aL= new ArrayList<>();
+        aL.add(gDV);
+        aL.add(gDV2);
+        return aL;
+    }
+
+    @Test
+    public void createAndRemoveDriverVehicle() throws SQLException {
+        GatewayDriverVehicle gDV =  insertDriverVehicle();
+        FinderDriverVehicle fDV = FinderDriverVehicle.getInstance();
+        GatewayDriverVehicle gDVTest = fDV.findByNumberPlateDriver(gDV.getUserDriver(), gDV.getnPVehicle());
+        assertNotNull(gDVTest);
+        assertEquals(gDV.getnPVehicle(), gDVTest.getnPVehicle());
+        assertEquals(gDV.getUserDriver(), gDVTest.getUserDriver());
+        gDV.remove();
+    }
+
+    @Test
+    public void createAndRemove2DriverVehicle() throws SQLException {
+        ArrayList<GatewayDriverVehicle> aL =  insertDriverVehicle2();
+        FinderDriverVehicle fDV = FinderDriverVehicle.getInstance();
+        assertEquals(1, fDV.findByNumberPlateV("TestV").size());
+        assertEquals(1, fDV.findByNumberPlateV("TestV2").size());
+        assertEquals(2, fDV.findByUserDriver("Test").size());
+        for (GatewayDriverVehicle gDV : aL) gDV.remove();
+
+    }
+
+}
