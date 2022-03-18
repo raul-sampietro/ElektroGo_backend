bf7af72990fc7d438235c38676997aac232b84e0
daniel.pulido.galvez <daniel.pulido.galvez@estudiantat.upc.edu>
2022-03-18 13:08:36 +0100

    Task#77_Actualitza VehicleController a la nova taula DriverVehicle

--- src/main/java/elektroGo/back/controller/VehicleController.java
+++ src/main/java/elektroGo/back/controller/VehicleController.java
@@ -1,28 +1,62 @@
 package elektroGo.back.controller;
 
+import elektroGo.back.data.Finders.FinderDriver;
+import elektroGo.back.data.Finders.FinderDriverVehicle;
 import elektroGo.back.data.Finders.FinderVehicle;
+import elektroGo.back.data.Gateways.GatewayDriverVehicle;
 import elektroGo.back.data.Gateways.GatewayVehicle;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 @RestController
 public class VehicleController {
 
     @PostMapping("/vehicle/create")
-    public void createVehicle(@RequestBody GatewayVehicle gV) {
+    public void createVehicle(@RequestBody GatewayVehicle gV, @RequestParam String userNDriver) {
+        FinderDriver fD = FinderDriver.getInstance();
         try {
-            gV.insert();
+            if (fD.findByUserName(userNDriver) != null) {
+                gV.insert();
+                GatewayDriverVehicle gDV = new GatewayDriverVehicle(gV.getNumberPlate(), userNDriver);
+            }
+            //Else throw an exception?
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    @GetMapping("/vehicle/read")
-    public String readVehicle(@RequestParam Long id) {
+    @PostMapping("/vehicle/addDriverVehicle")
+    public void addDriverVehicle(@RequestParam String nPVehicle, @RequestParam String userDriver) {
+        GatewayDriverVehicle gDV = new GatewayDriverVehicle(nPVehicle,userDriver);
+        try {
+            gDV.insert();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @PostMapping("/vehicle/removeDriverVehicle")
+    public void removeDriverVehicle(@RequestParam String nPVehicle, @RequestParam String userDriver) {
+        FinderDriverVehicle fDV = FinderDriverVehicle.getInstance();
+        try {
+            GatewayDriverVehicle gDV = fDV.findByNumberPlateDriver(userDriver, nPVehicle);
+            if ( gDV != null)
+                gDV.remove();
+            //Else throw an exception or something?
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+        @GetMapping("/vehicle/read")
+    public String readVehicle(@RequestParam String numberPlate) {
         FinderVehicle fV = FinderVehicle.getInstance();
         try {
-            GatewayVehicle gV = fV.findByID(id);
+            GatewayVehicle gV = fV.findByNumberPlate(numberPlate);
             if (gV == null) return null; //null is fine or show some http error or return something that front knows?
             return gV.json();
         }
@@ -32,11 +66,16 @@ public class VehicleController {
     }
 
     @PostMapping("/vehicle/delete")
-    public void deleteVehicle(@RequestParam Long id) {
+    public void deleteVehicle(@RequestParam String numberPlate) {
         FinderVehicle fV = FinderVehicle.getInstance();
         try {
-            GatewayVehicle gV = fV.findByID(id);
-            if (gV != null) gV.remove();
+            GatewayVehicle gV = fV.findByNumberPlate(numberPlate);
+            if (gV != null) {
+                FinderDriverVehicle fDV = FinderDriverVehicle.getInstance();
+                ArrayList<GatewayDriverVehicle> aL = fDV.findByNumberPlateV(numberPlate);
+                for (GatewayDriverVehicle gDV : aL) gDV.remove();
+                gV.remove();
+            }
             //Otherwise, show some http error?
         } catch (SQLException e) {
             e.printStackTrace();
