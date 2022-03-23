e53669e3900242082621e07353c22b794ed3f5ab
1251829 <adria.abad@estudiantat.upc.edu>
2022-03-23 12:21:13 +0100

    Task#94_Implementa la documentacio en Doxygen de DistanceCalculator.java

--- src/main/java/elektroGo/back/model/DistanceCalculator.java
+++ src/main/java/elektroGo/back/model/DistanceCalculator.java
@@ -1,3 +1,10 @@
+/**
+ * @file DistanceCalculator.java
+ * @author Adria Abad
+ * @date 22/03/2022
+ * @brief Implementacio de la classe DistanceCalculator.
+ */
+
 package elektroGo.back.model;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -12,13 +19,39 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 
+/**
+ * @brief La classe DistanceCalculator representa s'utilitza per calcular la matriu de distancies entre l'origen,
+ * el desti i les estacions de carrega
+ */
 public class DistanceCalculator {
+
+    /**
+     * @brief Capcelera de la url de la API de Google Maps
+     */
     private final String urlHeader = "https://maps.googleapis.com/maps/api/distancematrix/json";
+
+    /**
+     * @brief Contingut de la url que especifica els punts d'origen
+     */
     private String urlOrigins = "?origins=";
+
+    /**
+     * @brief Contingut de la url que especifica els punts de desti
+     */
     private String urlDestinations = "&destinations=";
+
+    /**
+     * @brief Contingut de la url que especifica la API KEY
+     */
     private String urlKey = "&key=";
 
-
+    /**
+     * @brief Funcio que crea la url de la API de Google Maps amb l'origen, el desti i les estacions de carrega
+     * @param ori Punt d'origen de la ruta
+     * @param dest Punt de desti de la ruta
+     * @param chargers Possibles punts de carrega pels quals s'haura de passar a la ruta
+     * @return Retorna la url creada
+     */
     private String createUrl(Point ori, Point dest, ArrayList<Point> chargers) {
         urlOrigins += ori.getLatitude().toString() + "%2C" + ori.getLongitude().toString();
         for (Point point : chargers) {
@@ -32,6 +65,14 @@ public class DistanceCalculator {
         return urlHeader + urlOrigins + urlDestinations + urlKey;
     }
 
+    /**
+     * @brief Funcio que utilitza la API de Google Maps per calcular la matriu de distancies entre l'origen, el desti
+     *  i les estacions de carrega
+     * @param ori Punt d'origen de la ruta
+     * @param dest Punt de desti de la ruta
+     * @param chargers Possibles punts de carrega pels quals s'haura de passar a la ruta
+     * @return Retorna la matriu de distancies
+     */
     public ArrayList<ArrayList<Integer>> calculateRoadDistanceMatrix(Point ori, Point dest, ArrayList<Point> chargers) {
         String url = createUrl(ori, dest, chargers);
         URI uri = URI.create(url);
@@ -46,6 +87,7 @@ public class DistanceCalculator {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        //Parsing the json object to the distance matrix
         ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
         for (int i = 0; i < rows.length(); ++i) {
             JSONArray jsonRow = rows.getJSONObject(i).getJSONArray("elements");
