10f5a6f11d00051232743fcde204d3f9a87832ef
1251829 <adria.abad@estudiantat.upc.edu>
2022-03-25 12:55:37 +0100

    Task#94_Implementa els tests de RouteCalculationTest.java

--- src/test/java/elektroGo/back/RouteCalculationTest.java
+++ src/test/java/elektroGo/back/RouteCalculationTest.java
@@ -6,26 +6,64 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.List;
 
 @SpringBootTest
 public class RouteCalculationTest {
 
     @Test
     public void routeWithoutStops() {
+        // For the given database of charging stations
+        int range = 150;
+        Double oriLat = 41.671230;
+        Double oriLon = 1.271296;
 
+        Double destLat = 41.929573;
+        Double destLon = 2.249060;
+
+        String expected = "";
+
+        RouteCalculation routeCalculation = new RouteCalculation(range, oriLat, oriLon, destLat, destLon);
+        routeCalculation.execute();
+        Point[] actualList = routeCalculation.getResult();
+        String actual = "";
+        for (Point p : actualList) {
+            actual += p.getLatitude() + " " + p.getLongitude();
+        }
+        assertEquals(expected, actual);
     }
 
     @Test
     public void routeWithOneStop() {
+        // For the given database of charging stations
+        int range = 85;
+        Double oriLat = 41.671230;
+        Double oriLon = 1.271296;
 
+        Double destLat = 41.929573;
+        Double destLon = 2.249060;
+
+        Point expected1 = new Point(41.733242, 1.845603);
+        Point[] expectedList = new Point[]{expected1};
+        String expected = "";
+        for (Point p : expectedList) {
+            expected += p.getLatitude() + " " + p.getLongitude();
+        }
+
+        RouteCalculation routeCalculation = new RouteCalculation(range, oriLat, oriLon, destLat, destLon);
+        routeCalculation.execute();
+        Point[] actualList = routeCalculation.getResult();
+        String actual = "";
+        for (Point p : actualList) {
+            actual += p.getLatitude() + " " + p.getLongitude();
+        }
+        assertEquals(expected, actual);
     }
 
     @Test
     public void routeWithSeveralStops() {
         // For the given database of charging stations
         int range = 150;
-        Double oriLat =  41.700801;
+        Double oriLat = 41.700801;
         Double oriLon = 2.847613;
 
         Double destLat = 40.705865;
