a5f1e6f31d06b49f6bbc5cb705bb307ac23a0f70
1251829 <adria.abad@estudiantat.upc.edu>
2022-04-13 14:14:19 +0200

    Task#143_Implementa el mecanisme de guardar els missatges a la base de dades

---
+++ src/main/java/elektroGo/back/controller/ChatsController.java
@@ -0,0 +1,51 @@
+/**
+ * @file ChatsController.java
+ * @author Adria Abad
+ * @date 13/04/2022
+ * @brief Implementacio de la classe ChatsController.
+ */
+
+package elektroGo.back.controller;
+
+import elektroGo.back.data.finders.FinderChats;
+import elektroGo.back.data.gateways.GatewayChats;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+//todo exceptions
+
+/**
+ * @brief La classe ChatsController és la classe que comunicarà front-end i back-end a l'hora de gestionar el chat
+ */
+@RestController
+@RequestMapping("/chat")
+public class ChatsController {
+
+    /**
+     * @brief Metode 'GET' que retorna tots els missatges de la base de dades
+     * @return Retorna el llistat de tots els missatges
+     */
+    @GetMapping("/findAll")
+    public ArrayList<GatewayChats> getAll(@RequestParam String userA, @RequestParam String userB) throws SQLException {
+        FinderChats fC = FinderChats.getInstance();
+        return fC.findAll();
+    }
+
+    /**
+     * @brief Metode 'GET' que retorna tots els missatges entre dos usuaris de la base de dades
+     * @param userA nom de l'usuari A
+     * @param userB nom de l'usuari B
+     * @return Retorna el llistat de missatges entre els dos usuaris ordenats per data
+     */
+    @GetMapping("/findByConversation")
+    public ArrayList<GatewayChats> getChatByConversation(@RequestParam String userA, @RequestParam String userB) throws SQLException {
+        FinderChats fC = FinderChats.getInstance();
+        return fC.findByConversation(userA, userB);
+    }
+
+}
---
+++ src/main/java/elektroGo/back/data/finders/FinderChats.java
@@ -0,0 +1,97 @@
+/**
+ * @file FinderChats.java
+ * @author Adria Abad
+ * @date 13/04/2022
+ * @brief Implementacio del Finder de Chats.
+ */
+
+package elektroGo.back.data.finders;
+
+import elektroGo.back.data.Database;
+import elektroGo.back.data.gateways.GatewayChats;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+/**
+ * @brief La classe FinderChats es l'encarregada de conectar-se amb la BD y retornar GW Chats.
+ */
+public class FinderChats {
+
+    /**
+     * @brief FinderChats, es un singleton
+     */
+    private static FinderChats singletonObject;
+
+    /**
+     * @brief Creadora de la classe FinderChats
+     */
+    private FinderChats() {}
+
+    /**
+     * @brief Funcio que retorna una instancia Singleton de FinderChats
+     * @return Es retorna un singletonObject per treballar amb aquesta classe
+     */
+    public static FinderChats getInstance() {
+        if (singletonObject == null) {
+            singletonObject = new FinderChats();
+        }
+        return singletonObject;
+    }
+
+    /**
+     * @brief Funció que agafa tots els Chats de la BD i els posa a un Array
+     * @return Es retorna un array de GatewayChats amb tota la info dels Chats
+     */
+    public ArrayList<GatewayChats> findAll() throws SQLException {
+        Database d = Database.getInstance();
+        Connection conn = d.getConnection();
+        ArrayList<GatewayChats> aL = new ArrayList<>();
+        PreparedStatement pS = conn.prepareStatement("SELECT * FROM CHATS;");
+        ResultSet r = pS.executeQuery();
+        while (r.next()) {
+            aL.add(this.createGateway(r));
+        }
+        return aL;
+    }
+
+    /**
+     * @brief Funció que agafa tots els missatges entre els dos usuaris indicats
+     * @param userA nom de l'usuari A
+     * @param userB nom de l'usuari B
+     * @return Es retorna un array de GatewayChats amb tota la info dels missatges ordenats per data
+     */
+    public ArrayList<GatewayChats> findByConversation(String userA, String userB) throws SQLException {
+        Database d = Database.getInstance();
+        Connection conn = d.getConnection();
+        PreparedStatement pS = conn.prepareStatement("" +
+                "SELECT * FROM CHATS WHERE sender = ? and receiver = ?" +
+                "UNION" +
+                "SELECT * FROM CHATS WHERE sender = ? and receiver = ?" +
+                "ORDER BY sentAt;");
+        pS.setString(1,userA);
+        pS.setString(2,userB);
+        pS.setString(3,userB);
+        pS.setString(4,userA);
+        ResultSet r = pS.executeQuery();
+        ArrayList<GatewayChats> aL = new ArrayList<>();
+        while (r.next()) {
+            aL.add(this.createGateway(r));
+        }
+        return aL;
+    }
+
+    /**
+     * @brief Crear un gateway amb els parametres passats
+     * @param r ResultSet que contindra tots el parametres per poder crear el Gateway (sender, receiver, message, createdAt)
+     * @return Retorna un GatewayChat amb els parametres donats
+     */
+    private GatewayChats createGateway(ResultSet r) throws SQLException {
+        String sender = r.getString(1);
+        String receiver = r.getString(2);
+        String message = r.getString(3);
+        Timestamp sentAt = r.getTimestamp(4);
+        return new GatewayChats(sender, receiver, message, sentAt);
+    }
+
+}
