45af75b52ed7c761cbe1c38cd262828e996f3be6
raul-sampietro <raul.sampietro@estudiantat.upc.edu>
2022-05-13 22:59:19 +0200

    Task#287_Updated API ChargingStations

--- src/main/java/elektroGo/back/controller/ChargingStationsController.java
+++ src/main/java/elektroGo/back/controller/ChargingStationsController.java
@@ -9,10 +9,8 @@ package elektroGo.back.controller;
 
 import elektroGo.back.data.finders.FinderChargingStations;
 import elektroGo.back.data.gateways.GatewayChargingStations;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -20,6 +18,7 @@ import java.util.ArrayList;
 /**
  * @brief La classe ChargingStationsController permet la comunicació entre back-end i front-end alhora de tractar les estacions de carrega
  */
+@RequestMapping("/charging-stations")
 @RestController
 public class ChargingStationsController {
 
@@ -27,21 +26,21 @@ public class ChargingStationsController {
      * @brief Metode 'GET' que retorna totes les estacions de càrrega de la base de dades
      * @return Retorna un array amb la informació de tots els punts de càrrega
      */
-    @GetMapping("/ChargingStations")
+    @GetMapping("")
     public ArrayList<GatewayChargingStations> getChargingStations() throws SQLException {
         FinderChargingStations fCS = FinderChargingStations.getInstance();
         return fCS.findAll();
     }
 
     /**
-     * @brief Metode 'GET' que retorna totes les estacions de càrrega que es trobin dins del quadrat/rectangle format per les coordenades donades
+     * @brief Metode 'GET' que retorna totes les estacions de càrrega que es trobin dins del rectangle format per les coordenades donades
      * @param latitude1 Latitiud de la primera coordenada
      * @param longitude1 Longitud de la primera coordenada
      * @param latitude2 Latitud de la segona coordenada
      * @param longitude2 Longitud de la segona coordenada
      * @return Retorna un array amb la informacio de tots els punts de carrega els quals estan situats dins de les coordenades donades
      */
-    @GetMapping("/ChargingStations/ByCoordinates")
+    @GetMapping("/byCoord")
     public ArrayList<GatewayChargingStations> getChargingStationByCoordinates(@RequestParam BigDecimal latitude1, BigDecimal longitude1, BigDecimal latitude2, BigDecimal longitude2) throws SQLException {
         FinderChargingStations fCS = FinderChargingStations.getInstance();
         return fCS.findByCoordinates(latitude1, longitude1, latitude2, longitude2);
