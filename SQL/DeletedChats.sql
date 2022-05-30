15f080742d0a544f04258ee2353b41d98b9ce902
1251829 <adria.abad@estudiantat.upc.edu>
2022-05-30 23:44:21 +0200

    tabla deleted chats

--- SQL/DeletedChats.sql
+++ SQL/DeletedChats.sql
@@ -2,4 +2,6 @@ CREATE TABLE DELETEDCHATS (
       UserA varchar(20),
       UserB varchar(20),
       CONSTRAINT DeletedChats_PK PRIMARY KEY (UserA, UserB),
+      CONSTRAINT DeletedChats_FK_Sender FOREIGN KEY (UserA) REFERENCES USERS(userName),
+      CONSTRAINT DeletedChats_FK_Receiver FOREIGN KEY (UserB) REFERENCES USERS(userName),
       CONSTRAINT NotSameUser CHECK (UserA <> UserB));
